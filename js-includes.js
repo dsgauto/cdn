d35e5f222956745cf720161beaa4d00c50f85060
Ibrahim Khalil <khalilmaster@hotmail.fr>
2022-11-20 23:17:06 -0800

    utm_term

--- js-includes.js
+++ js-includes.js
@@ -1,4 +1,10 @@
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const utm_source = urlParams.get('utm_source');
+    const utm_term = urlParams.get('utm_term');
     sessionStorage.setItem('utm_source', utm_source);
+    sessionStorage.setItem('utm_term', utm_term);
+
+
+
+    
